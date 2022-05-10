343c937b529f6fdf6506df5d4a2d2835d7937dd9
aboynamedjacob <74170306+aboynamedjacob@users.noreply.github.com>
2022-05-10 02:49:26 +0100

    Added SQL Database, Got rid of dummmy data and set up Dialog Close Listener class

--- app/src/main/java/com/example/to_do_app_dma/MainActivity.java
+++ app/src/main/java/com/example/to_do_app_dma/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.to_do_app_dma;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.example.to_do_app_dma.Adapter.ToDoAdapter;
@@ -10,21 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.to_do_app_dma.Utils.DataBaseHandler;
 import com.example.to_do_app_dma.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogCloseListener{
 
     private RecyclerView tasksRecyclerView;
     private ToDoAdapter tasksAdapter;
     private List<ToDoModel> taskList;
+    private DataBaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        db = new DataBaseHandler(this);
+        db.openDatabase();
 
         taskList = new ArrayList<>();
 
@@ -46,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
         taskList.add(task);
 
         tasksAdapter.setTasks(taskList);
+    }
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
     }
 }
 
